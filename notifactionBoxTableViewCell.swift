3c445c6e9cf6d3294d9309b60daa8e52fb4ed3d2
Peter <up806925@myport.ac.uk>
2019-04-21 17:10:45 +0100

    its done

---
+++ notifactionBoxTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  notifactionBoxTableViewCell.swift
+//  dismobUI
+//
+//  Created by user916839 on 4/16/19.
+//  Copyright © 2019 user916839. All rights reserved.
+//
+
+import UIKit
+
+class notifactionBoxTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
